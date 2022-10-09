970fda88a7fec3ebaf3781e6261a23321649a7df
Wes Copeland <wlcopeland1@gmail.com>
2022-10-09 13:24:58 -0400

    chore: set up dev playground

---
+++ src/__playground.ts
@@ -0,0 +1,9 @@
+console.log("retroachievements-api-js playground is running.");
+console.log(""); // newline
+
+/**
+ * PLEASE ONLY COMMIT CHANGES TO THIS FILE IF YOU WANT THEM
+ * TO DIRECTLY IMPACT EVERY DEV WORKING ON THE PROJECT.
+ *
+ * Use this file to test and experiment with changes to the project.
+ */
