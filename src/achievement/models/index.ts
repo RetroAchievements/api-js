ed039d2cd29dbed4f48ba22847c8cfd31c1b65d5
Wes Copeland <wlcopeland1@gmail.com>
2022-10-17 16:50:00 -0400

    fix(achievement): update exports

--- src/achievement/models/index.ts
+++ src/achievement/models/index.ts
@@ -1,4 +1,2 @@
-export * from "./achievement-of-the-week.model";
 export * from "./achievement-unlock-entity.model";
-export * from "./get-achievement-of-the-week-response.model";
 export * from "./get-achievement-unlocks-response.model";
