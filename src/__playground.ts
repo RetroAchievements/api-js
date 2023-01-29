62181819bc55d36ff6ba155ff0f6536fb453021b
Wes Copeland <wlcopeland1@gmail.com>
2023-01-29 14:15:43 -0500

    docs(playground): add more context

--- src/__playground.ts
+++ src/__playground.ts
@@ -3,6 +3,7 @@
  * TO DIRECTLY IMPACT EVERY DEV WORKING ON THE PROJECT.
  *
  * Use this file to test and experiment with changes to the project.
+ * If changes to this file land in a PR, you probably did something wrong.
  */
 
 // ---
