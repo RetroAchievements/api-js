f0560df365b1a5d03aca481d19c696a1da8a616e
Wes Copeland <wlcopeland1@gmail.com>
2023-11-04 14:38:42 -0400

    chore: migrate from jest to vitest (#49)

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    root: process.cwd(),
+
+    globals: true,
+
+    exclude: [
+      "node_modules",
+      ".next",
+      ".docusaurus",
+      "cypress",
+      "**/node_modules/**"
+    ],
+
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "**/*.model.ts",
+        "**/*.enum.ts",
+        "**/index.ts",
+        "**/test/**",
+        "**/__playground.ts"
+      ],
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
+});
