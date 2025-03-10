109c8b9a8157c701731ef6543112466eabaa8fe5
bram <deltabram@pm.me>
2025-03-09 20:02:22 -0400

    chore: remove old 'split' api functions

--- src/comments/index.ts
+++ src/comments/index.ts
@@ -1,5 +1,2 @@
 export * from "./getComments";
-export * from "./getCommentsOnAchievementWall";
-export * from "./getCommentsOnGameWall";
-export * from "./getCommentsOnUserWall";
 export * from "./models";
